a903f6cfa1f67a54869d19c554f75b6fc84252d4
guant <13258239832@163.com>
2024-03-23 15:08:16 +0800

    修改配置

---
+++ sharding-vertical-table/src/main/java/com/yl/VerticalTableApplication.java
@@ -0,0 +1,13 @@
+package com.yl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerticalTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerticalTableApplication.class, args);
+    }
+
+}
